6f106ff419ac5ddeae9712c233434d3a7f1ad8ef
spac3orange <m4gi3k44@proton.me>
2024-07-16 13:55:28 +0300

    v1

--- app/config/config_telethon.py
+++ app/config/config_telethon.py
@@ -6,21 +6,12 @@ from telethon import TelegramClient, errors, functions
 from environs import Env
 from telethon.tl.functions.messages import SendMessageRequest
 from telethon.tl.types import InputPeerChannel
-from .logger import logger
+from app.config.logger import logger
 from telethon.tl.functions.channels import JoinChannelRequest, GetFullChannelRequest
 import datetime
 from telethon.tl.functions.messages import GetHistoryRequest
 from datetime import timedelta, datetime
-from . import aiogram_bot, config_aiogram
-from telethon.tl.functions.account import UpdateProfileRequest, UpdateUsernameRequest
-from telethon.tl.functions.photos import UploadProfilePhotoRequest
-from telethon.tl.functions.users import GetFullUserRequest
-from telethon.errors import UsernameOccupiedError
-from telethon.tl.functions.photos import GetUserPhotosRequest, DeletePhotosRequest
-from telethon.tl.types import InputPhoto
-import aiofiles
-import re
-from telethon.tl.types import InputPeerChat
+from app.config import aiogram_bot, config_aiogram
 from environs import Env
 from app.crud import json_action
 from pprint import pprint
